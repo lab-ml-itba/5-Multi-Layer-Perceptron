a360d88872c4ca6624da348653cf5967eb51869e
cselmo <cselmo@itba.edu.ar>
2018-09-20 20:23:08 -0300

    softmax

--- MLP_helper.py
+++ MLP_helper.py
@@ -184,12 +184,18 @@ def test_options(X_train, y_train, X_test, y_test, options, plot_it=False):
         coefs_norm.append(np.linalg.norm(coefs))
     return degrees, lambdas, train_acc_array, test_acc_array, coefs_array_mean, coefs_array_std, coefs_abs_max, coefs_norm, coefs_num
 
-def plot_boundaries_keras(X_train, y_train, score, probability_func, degree=None, bias=False, h = .02, ax = None, margin=0.5):
+def plot_boundaries_keras(X_train, y_train, score, probability_func, model_vals=None,degree=None, bias=False, h = .02, ax = None, margin=0.5, mesh_res = 500,activation='sigmoid'):
+    colors=['g','w','y','b']
     X = X_train
-    x_min, x_max = X[:, 0].min() - margin, X[:, 0].max() + margin
-    y_min, y_max = X[:, 1].min() - margin, X[:, 1].max() + margin
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
+    margin_x = (X[:, 0].max() - X[:, 0].min())*margin
+    margin_y = (X[:, 1].max() - X[:, 1].min())*margin
+    x_min, x_max = X[:, 0].min() - margin_x, X[:, 0].max() + margin_x
+    y_min, y_max = X[:, 1].min() - margin_y, X[:, 1].max() + margin_y
+    hx = (x_max-x_min)/mesh_res
+    hy = (y_max-y_min)/mesh_res
+    x_domain = np.arange(x_min, x_max, hx)
+    y_domain = np.arange(y_min, y_max, hy)
+    xx, yy = np.meshgrid(x_domain, y_domain)
 
     if ax is None:
         ax = plt.subplot(1, 1, 1)
@@ -209,7 +215,8 @@ def plot_boundaries_keras(X_train, y_train, score, probability_func, degree=None
         Z = Zaux[:, 1]
     else:
         Z = Zaux[:, 0]
-
+        
+    
     # Put the result into a color plot
     Z = Z.reshape(xx.shape)
     
@@ -223,7 +230,22 @@ def plot_boundaries_keras(X_train, y_train, score, probability_func, degree=None
     # Plot also the training points
     ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
                edgecolors='k', s=100)
-
+    if activation=='tanh':
+        CT=0
+    elif activation=='sigmoid':
+        CT=0.5
+    elif activation=='relu':
+        CT=0.011
+    for num,model_val in enumerate(model_vals):
+        Z_th=list()
+        Zaux=model_val.predict(np.c_[xx.ravel(), yy.ravel()])
+        for idx in range(Zaux.shape[1]):
+            Z_th.append(Zaux[:,idx].reshape(xx.shape))
+        for z_th in Z_th:
+            boundary_line = np.where(np.abs(z_th-CT)<0.01)
+            ax.scatter(x_domain[boundary_line[1]], y_domain[boundary_line[0]], color=colors[num], alpha=0.5, s=10)
+    boundary_line = np.where(np.abs(Z-0.5)<0.01)
+    ax.scatter(x_domain[boundary_line[1]], y_domain[boundary_line[0]], color='k', alpha=0.5, s=10)
     ax.set_xlim(xx.min(), xx.max())
     ax.set_ylim(yy.min(), yy.max())
     ax.set_xticks(())
@@ -233,8 +255,9 @@ def plot_boundaries_keras(X_train, y_train, score, probability_func, degree=None
     
 from keras import regularizers
 from keras.initializers import RandomUniform
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers import Input
 from keras import optimizers
 from fnn_helper import PlotLosses
 from matplotlib import pyplot as plt
@@ -332,3 +355,74 @@ def get_basic_model(input_shape, output_size, lr=0.1):
                    ))
     model.compile(loss = 'binary_crossentropy', optimizer=optim, metrics=['accuracy'])
     return model
+
+def plot_th_MLP(hidden_units,activation='tanh', optim = optimizers.adam(lr=0.01, decay=0.0001),batch_size = 32, epochs = 10000, dropout=0.0, l2_lambda=0, initializer="normal", l1_lambda=0):
+    import numpy as np
+    X_train = np.load('X_train.npy')
+    X_test = np.load('X_test.npy')
+    y_train = np.load('y_train.npy') 
+    y_test = np.load('y_test.npy')
+    input_shape = 2
+    output_size = 1
+    plot_losses = PlotLosses(plot_interval=200, evaluate_interval=None, x_val=X_test, y_val_categorical=y_test)
+    if optim is None:
+        optim = optimizers.adam(lr=lr, decay=decay)
+    regularizer = regularizers.l1_l2(l1=l1_lambda, l2=l2_lambda)
+    model_vals=list()
+    if hidden_units:
+        input_model = Input(shape=(2,))
+        h1=Dense(hidden_units[0],
+                        input_dim=input_shape,  
+                        kernel_initializer=initializer, 
+                        bias_initializer=initializer,
+                        activation=activation,
+                        kernel_regularizer=regularizer, 
+                        bias_regularizer=regularizer
+                       )(input_model)
+        model_vals.append(Model(inputs=[input_model], outputs=[h1]))
+        d=Dropout(dropout)(h1)
+        for layer_hidden_units in hidden_units[1:]:
+            h=Dense(layer_hidden_units,  
+                        kernel_initializer=initializer, 
+                        bias_initializer=initializer,
+                        activation=activation,
+                        kernel_regularizer=regularizer, 
+                        bias_regularizer=regularizer
+                       )(d)
+            model_vals.append(Model(inputs=[input_model], outputs=[h]))
+            d=Dropout(dropout)(h)
+        h=Dense(output_size, 
+                        activation='sigmoid', 
+                        kernel_initializer=initializer, 
+                        bias_initializer=initializer,
+                        name='Salida',
+                        kernel_regularizer=regularizer, 
+                        bias_regularizer=regularizer
+                       )(d)
+        model = Model(inputs=[input_model], outputs=[h])
+        model.compile(loss = 'binary_crossentropy', optimizer=optim, metrics=['accuracy'])
+    else:
+        input_model = Input(shape=(2,))
+        h1=Dense(hidden_units[0],
+                        input_dim=input_shape,  
+                        kernel_initializer=initializer, 
+                        bias_initializer=initializer,
+                        activation=activation,
+                        kernel_regularizer=regularizer, 
+                        bias_regularizer=regularizer
+                       )(input_model)
+        model = Model(inputs=[input_model], outputs=[h1])
+        model_val= Model(inputs=[input_model], outputs=[h1])
+        model.compile(loss = 'binary_crossentropy', optimizer=optim, metrics=['accuracy'])
+    plot_losses = PlotLosses(plot_interval=200, evaluate_interval=None, x_val=X_test, y_val_categorical=y_test)
+    model.fit(X_train, 
+          y_train, batch_size = batch_size,
+          epochs=epochs, 
+          verbose=0, 
+          validation_data=(X_test, y_test), 
+          callbacks=[plot_losses],
+         )
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,6))
+    plot_boundaries_keras(X_train, y_train, model.evaluate(X_train, y_train)[1], model.predict, model_vals, h = 0.01, margin=0.1, ax=ax1, activation=activation)
+    plot_boundaries_keras(X_test, y_test, model.evaluate(X_test, y_test)[1], model.predict,model_vals, h = 0.01, margin=0.1, ax=ax2,  activation=activation)
+    plt.show()
